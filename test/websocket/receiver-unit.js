df02826f91e4f23cd878973e1face2eed9f349d0
Shaked Shauli <shaked.s@sealsecurity.io>
2026-04-01 20:29:24 +0300

    all

--- test/websocket/receiver-unit.js
+++ test/websocket/receiver-unit.js
@@ -1,13 +1,14 @@
 'use strict'
 
 const { test } = require('node:test')
+const assert = require('node:assert')
 const { ByteParser } = require('../../lib/web/websocket/receiver')
 const { states } = require('../../lib/web/websocket/constants')
 const { kController, kResponse } = require('../../lib/web/websocket/symbols')
 
 const invalidFrame = Buffer.from([0x82, 0x7F, 0x00, 0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01])
 
-test('ByteParser rejects 64-bit payload lengths with a non-zero upper word', (t) => {
+test('ByteParser rejects 64-bit payload lengths with a non-zero upper word', () => {
   const calls = {
     abort: 0
   }
@@ -30,7 +31,7 @@ test('ByteParser rejects 64-bit payload lengths with a non-zero upper word', (t)
 
   return new Promise((resolve) => {
     setImmediate(() => {
-      t.assert.strictEqual(calls.abort, 1)
+      assert.strictEqual(calls.abort, 1)
       parser.destroy()
       resolve()
     })
